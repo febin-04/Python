2decc34dd826ddd19af35cfea573f72b7abe37a4
febin-04 <febinnoble2028@cs.sjcetpalai.ac.in>
2024-10-01 14:52:32 +0530

    Add files via upload
    My first Commit

---
+++ Student_details.py
@@ -0,0 +1,8 @@
+print("Hello Python")
+name = input("Enter a name student : ")
+roll_no = int(input("Enter your roll number : "))
+roll_no = roll_no+1
+print('Your roll no is : ',roll_no)
+cgpa = float(input("Enter your cgpa : "))
+percentage = cgpa*10
+print(f"Marks percentage is :{percentage}%")
