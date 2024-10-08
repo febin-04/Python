c9a5df58a64eb262be85d60d2343a2569e4be2ee
febin-04 <febinnoble2028@cs.sjcetpalai.ac.in>
2024-10-08 14:41:42 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,20 @@
+'''
+Auther:Febin Noble
+Date:8-10-2024
+Program to create a calculator
+Version:1.0
+'''
+num1=int(input("Enter num1 : "))
+num2=int(input("Enter num2 : "))
+num3=int(input("Enter num3 : "))
+sum= num1+num2
+print(f"The sum of num1 and num2 is : {sum}")
+difference = num2-num1
+print(f"The difference when num2 is subtracted from num1 is: {difference}")
+product=num1*num2
+print(f"The product of num1 and num2 is: {product}")
+remainder=num1%num2
+print(f"The remainder when num1 is divided by num2 is {remainder}")
+result = (num1 + num2) * num3 / 2
+print(f"The result of (num1 + num2) * num3 / 2 is: {result}")
+
