25baea3bf3615ba0cd8651e1ba25a71e8587d955
febin-04 <febinnoble2028@cs.sjcetpalai.ac.in>
2024-10-15 14:48:04 +0530

    Add files via upload

---
+++ larger of 2 numbers.py
@@ -0,0 +1,12 @@
+"""
+Python project to find the larger of the two numbers
+Auther : Febin Noble
+Date : 15-10-2024
+Version 1.0
+"""
+num1=input("Enter num1 : ")
+num2=input("Enter num2 : ")
+if num1>num2:
+    print("num1 is the larger of the two numbers")
+else:
+    print("num2 is the greater of the two numbers")
