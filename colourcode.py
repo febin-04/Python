5b1476f090d9ea0813735a8215c748a04da10af8
febin-04 <febinnoble2028@cs.sjcetpalai.ac.in>
2024-10-15 14:50:15 +0530

    Add files via upload

---
+++ colourcode.py
@@ -0,0 +1,18 @@
+"""
+Python program to indentify color based on grade
+Auther : Febin Noble
+Date : 15-10-2024
+Version 1.0
+"""
+grade=input("Enter your grade : ")
+if grade == "R":
+    print("Your color is Red")
+elif grade =="G":
+    print("Your color is Green")
+elif grade == "B":
+    print("Your color is Blue")
+else:
+    print("Wrong code")
+
+
+
