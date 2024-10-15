0f51f110fd82c472d3c4ecbf2577ac4e76755b5d
febin-04 <febinnoble2028@cs.sjcetpalai.ac.in>
2024-10-15 16:11:27 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,12 @@
+"""
+Python program to get the odd numbers upto the given number
+Auther : Febin Noble
+ Date; 15-20-24
+ Version : 1.0
+
+"""
+limit = int(input("Enter the limit:"))
+odd_number=1
+while odd_number<limit:
+    print(odd_number ,"\t" ,  end="" )
+    odd_number+=2
