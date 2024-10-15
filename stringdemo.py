01d361eba22ac7fe0aa6558bed692af103127bb4
febin-04 <febinnoble2028@cs.sjcetpalai.ac.in>
2024-10-15 14:04:35 +0530

    Add files via upload

---
+++ stringdemo.py
@@ -0,0 +1,11 @@
+'''
+Python program to check whether the given number  is positive or negative
+Auther : Febin Noble
+Date: 15-10-2024
+Version: 1.0
+'''
+number = int(input("Enter a number : "))
+if number>0:
+    print(f"The given number : {number} is positive")
+else:
+    print(f"The given  number : {number} is negative")
