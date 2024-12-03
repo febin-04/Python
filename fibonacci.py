c2a0c663ecdcacbccdadfa3783b96e42f8d15b82
febin-04 <febinnoble2028@cs.sjcetpalai.ac.in>
2024-12-03 14:23:06 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,8 @@
+def fibonacci(n):
+    sequence =[]
+    first_number,second_number=0,1
+    for i  in range(n):
+        sequence.append(first_number)
+        first_number,second_number=second_number,first_number+second_number
+    return sequence
+print(fibonacci(10))
